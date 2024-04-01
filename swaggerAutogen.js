cdf5771dafc0462cfb5f9225e2dd367c3279e3c2
ensar <ensarsagkal@gmail.com>
2024-04-01 22:37:00 +0300

    up

--- swaggerAutogen.js
+++ swaggerAutogen.js
@@ -41,6 +41,8 @@ const document = {
 	definitions: {
 		// Models:
 		"User": require('./src/models/user').schema.obj,
+		"Car": require('./src/models/car').schema.obj,
+		"Reservation": require('./src/models/reservation').schema.obj,
 	}
 }
 
