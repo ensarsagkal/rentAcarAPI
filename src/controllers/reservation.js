cc8dd0afdcda1bbf6ff3a78703ad8e57ef98e8ba
ensar <ensarsagkal@gmail.com>
2024-04-01 22:38:31 +0300

    up

--- src/controllers/reservation.js
+++ src/controllers/reservation.js
@@ -3,3 +3,102 @@
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
 // Reservation Controller:
+
+const Reservation = require('../models/reservation')
+
+module.exports = {
+
+    list: async (req, res) => {
+        /*
+            #swagger.tags = ["Reservations"]
+            #swagger.summary = "List Reservations"
+            #swagger.description = `
+                You can send query with endpoint for filter[], search[], sort[], page and limit.
+                <ul> Examples:
+                    <li>URL/?<b>filter[field1]=value1&filter[field2]=value2</b></li>
+                    <li>URL/?<b>search[field1]=value1&search[field2]=value2</b></li>
+                    <li>URL/?<b>sort[field1]=1&sort[field2]=-1</b></li>
+                    <li>URL/?<b>page=2&limit=1</b></li>
+                </ul>
+            `
+        */
+
+        const data = await res.getModelList(Reservation)
+
+        res.status(200).send({
+            error: false,
+            details: await res.getModelListDetails(Reservation),
+            data
+        })
+    },
+
+    create: async (req, res) => {
+        /*
+            #swagger.tags = ["Reservations"]
+            #swagger.summary = "Create Reservation"
+            #swagger.parameters['body'] = {
+                in: 'body',
+                required: true,
+                schema: {
+                    $ref: '#/definitions/Reservation'
+                }
+            }
+        */
+
+        const data = await Reservation.create(req.body)
+
+        res.status(201).send({
+            error: false,
+            data
+        })
+    },
+
+    read: async (req, res) => {
+        /*
+            #swagger.tags = ["Reservations"]
+            #swagger.summary = "Get Single Reservation"
+        */
+
+        res.status(200).send({
+            error: false,
+            data
+        })
+
+    },
+
+    update: async (req, res) => {
+        /*
+            #swagger.tags = ["Reservations"]
+            #swagger.summary = "Update Reservation"
+            #swagger.parameters['body'] = {
+                in: 'body',
+                required: true,
+                schema: {
+                    $ref: '#/definitions/Reservation'
+                }
+            }
+        */
+
+        res.status(202).send({
+            error: false,
+            data,
+            new: await Reservation.findOne({ _id: req.params.id })
+        })
+
+    },
+
+    delete: async (req, res) => {
+        /*
+            #swagger.tags = ["Reservations"]
+            #swagger.summary = "Delete Reservation"
+        */
+
+        const data = await Reservation.deleteOne({ _id: req.params.id })
+
+        res.status(data.deletedCount ? 204 : 404).send({
+            error: !data.deletedCount,
+            data
+        })
+
+    },
+}
--- src/controllers/user.js
+++ src/controllers/user.js
@@ -15,6 +15,7 @@ module.exports = {
             #swagger.description = `
                 You can send query with endpoint for search[], sort[], page and limit.
                 <ul> Examples:
+                    <li>URL/?<b>filter[field1]=value1&filter[field2]=value2</b></li>
                     <li>URL/?<b>search[field1]=value1&search[field2]=value2</b></li>
                     <li>URL/?<b>sort[field1]=1&sort[field2]=-1</b></li>
                     <li>URL/?<b>page=2&limit=1</b></li>
@@ -48,10 +49,7 @@ module.exports = {
                 }
             }
         */
-        /* EĞER login olan kullanıcı admin değilse post işleminde yetkileri false  
-        req.body.isStaff=false
-        req.body.isAdmin=false
-        */
+       
         const data = await User.create(req.body)
 
         res.status(201).send({
@@ -65,16 +63,6 @@ module.exports = {
             #swagger.tags = ["Users"]
             #swagger.summary = "Get Single User"
         */
-       
-        //? Yetkisiz kullanıcının başka bir kullanıcıyı yönetmesini engelle (sadece kendi verileri):
-        // if (!req.user.isAdmin) {
-        //     req.params.id = req.user.id
-        // }
-        // const data = await User.findOne({ _id: req.params.id })
-
-        //? Yetkisiz kullanıcının başka bir kullanıcıyı yönetmesini engelle (sadece kendi verileri):
-        const id = req.user.isAdmin ? req.params.id : req.user.id
-        const data = await User.findOne({ _id: id })
 
         res.status(200).send({
             error: false,
@@ -100,10 +88,6 @@ module.exports = {
                 }
             }
         */
-
-        //? Yetkisiz kullanıcının başka bir kullanıcıyı yönetmesini engelle (sadece kendi verileri):
-        if (!req.user.isAdmin) req.params.id = req.user._id
-        const data = await User.updateOne({ _id: req.params.id }, req.body, { runValidators: true })
 
         res.status(202).send({
             error: false,
