64d4bb2837038644bf233368b6577767d0988d87
ensar <ensarsagkal@gmail.com>
2024-04-02 14:11:51 +0300

    up

--- src/helpers/sync.js
+++ src/helpers/sync.js
@@ -4,14 +4,27 @@
 ------------------------------------------------------- */
 // sync():
 
-module.exports = async function() {
+module.exports = async function () {
 
-    return null;
+    // return null;
 
     /* REMOVE DATABASE */
     const { mongoose } = require('../configs/dbConnection')
     await mongoose.connection.dropDatabase()
     console.log('- Database and all data DELETED!')
     /* REMOVE DATABASE */
+
+    const User = require('../models/user')
+    await User.create({
+        "username": "admin",
+        "password": "aA?123456",
+        "email": "admin@site.com",
+        "firstName": "admin",
+        "lastName": "admin",
+        "isActive": true,
+        "isStaff": true,
+        "isAdmin": true
+    })
+    console.log('- Admin user created.')
 
 }
