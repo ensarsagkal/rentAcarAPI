36379e57c5b33f4321ea2b4aa3bfd20b18cbf240
ensar <ensarsagkal@gmail.com>
2024-04-07 17:48:09 +0300

    up

--- src/controllers/reservation.js
+++ src/controllers/reservation.js
@@ -1,50 +1,40 @@
-"use strict"
-/* -------------------------------------------------------
-    NODEJS EXPRESS | CLARUSWAY FullStack Team
-------------------------------------------------------- */
-// Reservation Controller:
+"use strict";
 
-const Reservation = require('../models/reservation')
+const Reservation = require("../models/reservation");
+const Car = require("../models/car")
 
 module.exports = {
-
-    list: async (req, res) => {
-        /*
+  list: async (req, res) => {
+     /*
             #swagger.tags = ["Reservations"]
             #swagger.summary = "List Reservations"
             #swagger.description = `
-                You can send query with endpoint for filter[], search[], sort[], page and limit.
+                You can send query with endpoint for search[], sort[], page and limit.
                 <ul> Examples:
-                    <li>URL/?<b>filter[field1]=value1&filter[field2]=value2</b></li>
                     <li>URL/?<b>search[field1]=value1&search[field2]=value2</b></li>
                     <li>URL/?<b>sort[field1]=1&sort[field2]=-1</b></li>
                     <li>URL/?<b>page=2&limit=1</b></li>
                 </ul>
             `
         */
-
-        // Başka bir kullanıcı datasını görmesini engelle:
-        let customFilter = {}
-        if (!req.user.isAdmin && !req.user.isStaff) {
-            customFilter = { userId: req.user._id }
-        }
-
-        const data = await res.getModelList(Reservation, customFilter, [
-            { path: 'userId', select: 'username firstName lastName' },
-            { path: 'carId' },
-            { path: 'createdId', select: 'username' },
-            { path: 'updatedId', select: 'username' },
-        ])
-
-        res.status(200).send({
-            error: false,
-            details: await res.getModelListDetails(Reservation, customFilter),
-            data
-        })
-    },
-
-    create: async (req, res) => {
-        /*
+    let filters = {};
+    if (!req.user.isAdmin && !req.user.isStaff) {
+      filters = { userId: req.user._id };
+    }
+    const data = await res.getModelList(Reservation, filters, [
+      { path: "userId", select: "username lastName firstName" },
+      { path: "carId" },
+      { path: "createdId", select: "username " },
+      { path: "createdId", select: "username " },
+    ]);
+    res.status(200).send({
+      error: false,
+      details: await res.getModelListDetails(Reservation, filters),
+      data,
+    });
+  },
+  create: async (req, res) => {
+       /*
             #swagger.tags = ["Reservations"]
             #swagger.summary = "Create Reservation"
             #swagger.parameters['body'] = {
@@ -56,114 +46,101 @@ module.exports = {
             }
         */
 
-        // "Admin/staf değilse" veya "UserId göndermişmemişse" req.user'dan al:
-        if ((!req.user.isAdmin && !req.user.isStaff) || !req.body?.userId) {
-            req.body.userId = req.user._id
-        }
+    if ((!req.user.isAdmin && !req.user.isStaff) || !req.body?.userId) {
+      req.body.userId = req.user._id;
+    }
+    req.body.createdId = req.user._id;
+    req.body.updatedId = req.user._id;
+    if (!req.body.amount) {
+        const carData = await Car.findOne({ _id: req.body.carId });
+        const startDate = new Date(req.body.startDate);
+        const endDate = new Date(req.body.endDate);
+        const days = (endDate - startDate) / (1000 * 60 * 60 * 24);
+        req.body.amount = carData.pricePerDay * days;
+      }
+    const userReservationInDates = await Reservation.findOne({
+      userId: req.body.userId,
+      carId: req.body.carId,
+      $nor: [
+        { startDate: { $gt: req.body.endDate } },
+        { endDate: { $lt: req.body.startDate } },
+      ],
+    });
+    if (userReservationInDates) {
+      res.errorStatusCode = 400;
+      throw new Error(
+        "It cannot be added because there is another reservation with the same date.",
+        { cause: { userReservationInDates: userReservationInDates } }
+      );
+    } else {
+      const data = await Reservation.create(req.body);
+      res.status(201).send({
+        error: false,
+        data,
+      });
+    }
+  },
 
-        // createdId ve updatedId verisini req.user'dan al:
-        req.body.createdId = req.user._id
-        req.body.updatedId = req.user._id
+  read: async (req, res) => {
+     /*
+        #swagger.tags = ["Reservations"]
+        #swagger.summary = "Get Single Reservation"
+    */
+    let filters = {};
+    if (!req.user.isAdmin && !req.user.isStaff) {
+      filters = { userId: req.user.id };
+    }
+    const data = await Reservation.findOne({ _id: req.params.id,...filters }).populate([
+        { path: 'userId', select: 'username firstName lastName' },
+        { path: 'carId' },
+        { path: 'createdId', select: 'username' },
+        { path: 'updatedId', select: 'username' },
+    ]);
+    res.status(200).send({
+      error: false,
+      data,
+    });
+  },
+  update: async (req, res) => {
 
-        // Kullanıcının çakışan tarihlerde başka bir reservesi var mı?
-        const userReservationInDates = await Reservation.findOne({
-            userId: req.body.userId,
-            // carId: req.body.carId, // Farklı bir araba kiralanabilir
-            $nor: [
-                { startDate: { $gt: req.body.endDate } }, // gt: >
-                { endDate: { $lt: req.body.startDate } } // lt: <
-            ]
-        })
-        // console.log(userReservationInDates)
+    // if (!req.body.amount) {
+    //     const carData = await Car.findOne({ _id: req.body.carId });
+    //     const startDate = new Date(req.body.startDate);
+    //     const endDate = new Date(req.body.endDate);
+    //     const days = (endDate - startDate) / (1000 * 60 * 60 * 24);
+    //     req.body.amount = carData.pricePerDay * days;
+    //   }
+    let filters={}
+    req.body.updatedId = req.user._id
+    if (!req.user.isAdmin && !req.user.isStaff) {
+        filters.userId = req.user._id;
+    }
+    const data = await Reservation.updateOne( filters , req.body, {
+      runValidators: true,
+    });
 
-        if (userReservationInDates) {
+    res.status(202).send({
+      error: false,
+      data,
+      new: await Reservation.findOne({ _id: req.params.id }),
+    });
+  },
+  delete: async (req, res) => {
+    /*
+        #swagger.tags = ["Reservations"]
+        #swagger.summary = "Delete Reservation"
+    */
 
-            res.errorStatusCode = 400
-            throw new Error(
-                'It cannot be added because there is another reservation with the same date.',
-                { cause: { userReservationInDates: userReservationInDates } }
-            )
-
-        } else {
-
-            const data = await Reservation.create(req.body)
-
-            res.status(201).send({
-                error: false,
-                data
-            })
-        }
-    },
-
-    read: async (req, res) => {
-        /*
-            #swagger.tags = ["Reservations"]
-            #swagger.summary = "Get Single Reservation"
-        */
-
-        // Başka bir kullanıcı datasını görmesini engelle:
-        let customFilter = {}
-        if (!req.user.isAdmin && !req.user.isStaff) {
-            customFilter = { userId: req.user._id }
-        }
-
-        const data = await Reservation.findOne({ _id: req.params.id, ...customFilter }).populate([
-            { path: 'userId', select: 'username firstName lastName' },
-            { path: 'carId' },
-            { path: 'createdId', select: 'username' },
-            { path: 'updatedId', select: 'username' },
-        ])
-
-        res.status(200).send({
-            error: false,
-            data
-        })
-
-    },
-
-    update: async (req, res) => {
-        /*
-            #swagger.tags = ["Reservations"]
-            #swagger.summary = "Update Reservation"
-            #swagger.parameters['body'] = {
-                in: 'body',
-                required: true,
-                schema: {
-                    $ref: '#/definitions/Reservation'
-                }
-            }
-        */
-
-        // Admin değilse rezervasyona ait userId değiştirilemez:
-        if (!req.user.isAdmin) {
-            delete req.body.userId
-        }
-
-        // updatedId verisini req.user'dan al:
-        req.body.updatedId = req.user._id
-
-        const data = await Reservation.updateOne({ _id: req.params.id }, req.body, { runValidators: true })
-
-        res.status(202).send({
-            error: false,
-            data,
-            new: await Reservation.findOne({ _id: req.params.id })
-        })
-
-    },
-
-    delete: async (req, res) => {
-        /*
-            #swagger.tags = ["Reservations"]
-            #swagger.summary = "Delete Reservation"
-        */
-
-        const data = await Reservation.deleteOne({ _id: req.params.id })
-
-        res.status(data.deletedCount ? 204 : 404).send({
-            error: !data.deletedCount,
-            data
-        })
-
-    },
-}
+    let filters={}
+    req.body.updatedId = req.user._id
+    if (!req.user.isAdmin && !req.user.isStaff) {
+        filters.userId = req.user._id;
+    }
+    const data = await Reservation.deleteOne({...filters});
+  
+    res.status(data.deletedCount ? 204 : 404)({
+      error: !data.deletedCount,
+      data,
+    });
+  },
+};
